476ca2f61f7cdaedb06a5b920c1c101461fbbc04
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-06 16:31:09 -0500

    array exercises done

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,19 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] numArray = { 1, 1, 2, 3, 5, 8 };
+
+            foreach (int number in numArray)
+            {
+                if (number % 2 == 1)
+                {
+                    Console.WriteLine(number);
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
