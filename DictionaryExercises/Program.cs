1009f4c31999bfdfebedb22db98dd03313b1cae6
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-10 17:17:12 -0500

    dictionary exercises almost complete

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,41 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            int idNum;
+
+            Console.WriteLine("Enter your student ID number (or ENTER to finish):");
+
+            // Get student names and grades
+            do
+            {
+                Console.WriteLine("ID number: ");
+                string input = Console.ReadLine();
+                int changeInputToNum = int.Parse(input);
+                idNum = changeInputToNum;
+
+                if (!Equals(idNum, ""))
+                {
+                    Console.WriteLine("Name of Student: ");
+                    string input2 = Console.ReadLine();
+                    string nameOfStudent = input2;
+
+                    students.Add(idNum, nameOfStudent);
+
+                    // Read in the newline before looping back
+                    
+                }
+
+            } while (!Equals(idNum, ""));
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Key + ": " + student.Value);
+            }
         }
     }
 }
